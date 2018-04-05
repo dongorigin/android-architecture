bb73d9703b39c3c1322a173287f544252458ad6a
dong <dongorigin@gmail.com>
2018-04-05 17:44:57 +0800

    Paging 1.0.0-alpha7

--- app/src/main/java/cn/dong/architecture/data/net/StoryDataSourceFactory.kt
+++ app/src/main/java/cn/dong/architecture/data/net/StoryDataSourceFactory.kt
@@ -7,7 +7,7 @@ import cn.dong.architecture.data.model.Story
 /**
  * @author dong on 2018/03/11.
  */
-class StoryDataSourceFactory : DataSource.Factory<String, Story> {
+class StoryDataSourceFactory : DataSource.Factory<String, Story>() {
 
     val sourceLiveData = MutableLiveData<StoryDataSource>()
 
